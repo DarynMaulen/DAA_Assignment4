c0545426b4daee5f6b2e85b602f7b48c0880ca9d
Дарын <241245@astanait.edu.kz>
2025-10-29 16:18:32 +0300

    add node, edge, graph models and json parser class

---
+++ src/main/java/graph/model/Edge.java
@@ -0,0 +1,29 @@
+package graph.model;
+
+public class Edge {
+    private final int u;
+    private final int v;
+    private final Integer weight = null;
+
+    public Edge(int u, int v, Integer weight) {
+        this.u = u;
+        this.v = v;
+    }
+
+    public int getU() {
+        return u;
+    }
+
+    public int getV() {
+        return v;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Edge { u= " + u + ", v= " + v + ", weight= " + weight + " }";
+    }
+}
